d7f2dc0d72ab56fdf696549d60b0a139f169db41
Cyanineee-24 <scoutchampion123@gmail.com>
2026-02-28 03:40:41 +0800

    feat(api): add database logic and storing

--- api/logic.py
+++ api/logic.py
@@ -3,20 +3,51 @@ import json
 import os
 import io
 from dotenv import load_dotenv
+from supabase import create_client, Client
 import google.generativeai as genai
 from PIL import Image
 
-# 1. Load Environment & API Keys
+
+# 1. Initialize Supabase Client
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+# 2. Load Environment & API Keys
 load_dotenv()
 my_secret_key = os.getenv("GEMINI_API_KEY")
 if not my_secret_key:
-    raise ValueError("CRITICAL ERROR: GEMINI_API_KEY is missing. Check your .env")
+    raise ValueError(
+        "CRITICAL ERROR: GEMINI_API_KEY is missing. Check your .env")
 
-# 2. Configure Gemini
+# 3. Configure Gemini
 genai.configure(api_key=my_secret_key)
 model = genai.GenerativeModel('gemini-2.5-flash')
 
-# 3. The Core Function
+# 4. Save to DB logic
+
+
+def save_to_database(data: dict):
+    """Inserts the extracted resibo data into Supabase."""
+
+    try:
+        # Use the data keys that match SQL table columns
+        response = supabase.table("resibo_ledger").insert({
+            "reference_number": data.get("reference_number"),
+            "amount": data.get("amount"),
+            "sender_name": data.get("sender_name"),
+            "transaction_date": data.get("timestamp"),
+            "fingerprint": data.get("fingerprint")
+        }).execute()
+
+        return response
+
+    except Exception as e:
+        print(f"Database Error: {e}")
+        raise e
+
+
+# 5. The Core Function
 def process_receipt_image(image_bytes: bytes) -> dict:
     """Takes image bytes, extracts data via Gemini, and signs it."""
     img = Image.open(io.BytesIO(image_bytes))
@@ -30,8 +61,9 @@ def process_receipt_image(image_bytes: bytes) -> dict:
     Return ONLY the raw JSON. No markdown blocks, no explanations."""
 
     response = model.generate_content([prompt, img])
-    
-    clean_json = response.text.replace('```json', '').replace('```', '').strip()
+
+    clean_json = response.text.replace(
+        '```json', '').replace('```', '').strip()
     data = json.loads(clean_json)
 
     # Generate the Tamper-Proof Hash
@@ -39,5 +71,6 @@ def process_receipt_image(image_bytes: bytes) -> dict:
     digital_signature = hashlib.sha256(raw_signature.encode()).hexdigest()
 
     data['fingerprint'] = digital_signature
+    save_to_database(data)
 
     return data
